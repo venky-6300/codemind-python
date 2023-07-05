219a8a44ddd65e4185fa1eec38f2e45b46dba450
venky-6300 <117163362+venky-6300@users.noreply.github.com>
2023-07-05 15:14:41 +0530

    n=int(input()) b=[] c=1 while n:     a=n%10     n=n//10     b.append(a)     c=c*a if sum(b)==c:     print('Spy Number') else:     print('Not Spy Number')

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,11 +1,14 @@
-n = int(input())
-sum = 0
-pro = 1
+n=int(input())
+b=[]
+c=1
 while n:
-    sum+=n%10
-    pro*=n%10
-    n//=10
-if sum==pro:
-    print("Spy Number")
+    a=n%10
+    n=n//10
+    b.append(a)
+    c=c*a
+if sum(b)==c:
+    print('Spy Number')
 else:
-    print("Not Spy Number")
+    print('Not Spy Number')
+    
+    
